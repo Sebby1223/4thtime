2eb7fbd247fc064379cb26c922e1bdc822612dfd
Sebby1223 <111583961+Sebby1223@users.noreply.github.com>
2024-05-14 21:51:50 -0400

    w

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,11 +73,13 @@
   /**
    * Team buttons
    */
-  const teamMembers = document.querySelectorAll('.team-member');
-
-  teamMembers.forEach(member => {
-    member.addEventListener('click', () => {
-      member.classList.toggle('flipped');
+  document.addEventListener('DOMContentLoaded', function() {
+    const teamMembers = document.querySelectorAll('.team-member');
+    
+    teamMembers.forEach(member => {
+      member.addEventListener('click', () => {
+        member.classList.toggle('flipped');
+      });
     });
   });
   /**
